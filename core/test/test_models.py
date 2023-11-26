636e95416cbabdefaf7f963e284d4463a2e66069
Ossy Lab <ossyisibor@gmail.com>
2023-11-26 14:28:15 +0000

    Setup and Connect with DB

---
+++ core/test/test_models.py
@@ -0,0 +1,24 @@
+"""
+Test for models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test models"""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email is successful."""
+        email = 'test@example.com'
+        password = 'testpassword12'
+        username = 'testuser'
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+            username=username
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
